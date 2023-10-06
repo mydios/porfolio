4a291b86555bf104cf3820c4c3a17848655d65f8
Dylan Van Parys <dylan.van.parys@telenet.be>
2023-10-06 17:46:18 +0200

    update home page

--- theme.config.js
+++ theme.config.js
@@ -1,9 +1,12 @@
+import Link from "next/link"
+
 const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Dylan Van Parys.
+      <p>This portfolio is built with <b>Next.js</b> and a library called <Link href="https://nextra.vercel.app/">Nextra</Link>. It allows you to write Markdown and focus on the content of your portfolio.</p>
       <style jsx>{`
         a {
           float: right;
